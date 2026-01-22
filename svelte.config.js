4f7b2165cb8665e73bc49aa34d45a27e69863991
code0xff(WM) <ian.jungyong.um@gmail.com>
2026-01-22 11:42:31 +0900

    build: prepare github page deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,12 +8,16 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		adapter: adapter({ fallback: 'index.html' }),
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
 			'@/*': './src/lib/*'
+		},
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/eth-passkey' : ''
 		}
 	}
 };
